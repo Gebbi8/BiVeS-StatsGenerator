8f98a473b0adc8b7dfa24ff8889e73e0959bb914
Martin Scharm <martin@binfalse.de>
2016-11-20 15:11:22 +0100

    created an all-in-one-boxplot figure

--- src/main/resources/boxplots.R
+++ src/main/resources/boxplots.R
@@ -58,6 +58,113 @@ boxit<-function (table, type)
 
 }
 
+
+
+allinone<-function (SBML, CellML)
+{
+	
+	insertsSBML=SBML[,5]
+	deletesSBML=SBML[,6]
+	movesSBML=SBML[,7]
+	updatesSBML=SBML[,8]
+	
+	updatesSBML=updatesSBML[updatesSBML!=0]
+	deletesSBML=deletesSBML[deletesSBML!=0]
+	insertsSBML=insertsSBML[insertsSBML!=0]
+	movesSBML=movesSBML[movesSBML!=0]
+	
+	nodesSBML=SBML[,13]
+	attributesSBML=SBML[,14]
+	textsSBML=SBML[,15]
+	
+	nodesSBML=nodesSBML[nodesSBML!=0]
+	attributesSBML=attributesSBML[attributesSBML!=0]
+	textsSBML=textsSBML[textsSBML!=0]
+	
+	
+	
+	insertsCellML=CellML[,5]
+	deletesCellML=CellML[,6]
+	movesCellML=CellML[,7]
+	updatesCellML=CellML[,8]
+	
+	updatesCellML=updatesCellML[updatesCellML!=0]
+	deletesCellML=deletesCellML[deletesCellML!=0]
+	insertsCellML=insertsCellML[insertsCellML!=0]
+	movesCellML=movesCellML[movesCellML!=0]
+	
+	nodesCellML=CellML[,13]
+	attributesCellML=CellML[,14]
+	textsCellML=CellML[,15]
+	
+	nodesCellML=nodesCellML[nodesCellML!=0]
+	attributesCellML=attributesCellML[attributesCellML!=0]
+	textsCellML=textsCellML[textsCellML!=0]
+	
+	
+	#pdf("operation-types.pdf",width=10,height=6)
+	
+	#boxplot(updates,deletes,inserts,moves,horizontal=T,log='x',ylim=c(1,1000),names=c("updates","deletes","inserts","moves"),col=c(7,2:4))
+	
+	#boxplot(nodes,attributes,texts,horizontal=T,log='x',ylim=c(1,1000),names=c("nodes","attributes","texts"),col=c(7,2:4))
+	
+	
+	# 	print (updates)
+	
+	pdf ("./data/graphs/boxes/allinone.pdf",width=8,height=7)
+	
+	
+	oldpar=par(mar=c(7,1.3,0.2,0)+.1,mfrow=c(1,4))
+	
+	boxplot(updatesSBML,deletesSBML,insertsSBML,movesSBML,horizontal=F,log='y',ylim=c(1,1000),
+					#names=c("updates","deletes","inserts","moves","nodes","attributes","texts"),
+					col=c("#FFFA5F","#f47e7d","#b5d045","#83C5D1"), yaxt='n')
+	axis(1, at=1:4,c("updates","deletes","inserts","moves"),las=2,cex.axis=.9)
+# 	par(oldpar)
+	
+	
+	par(mar=c(7,0,0.2,1.3)+.1)
+	boxplot(updatesCellML,deletesCellML,insertsCellML,movesCellML,horizontal=F,log='y',ylim=c(1,1000),
+					#names=c("updates","deletes","inserts","moves","nodes","attributes","texts"),
+					col=c("#FFFA5F","#f47e7d","#b5d045","#83C5D1"), yaxt='n')
+	axis(1, at=1:4,c("updates","deletes","inserts","moves"),las=2,cex.axis=.9)
+	# 	par(oldpar)
+	
+	axis(4,labels=F)
+	
+	
+	
+	
+	par(mar=c(7,1.3,0.2,0)+.1)
+# 	oldpar=par(mar=c(2,6.5,0.2,0)+.1,mfrow=c(1,1))
+	boxplot(nodesSBML,attributesSBML,textsSBML,horizontal=F,log='y',ylim=c(1,1000),
+					#names=c("updates","deletes","inserts","moves","nodes","attributes","texts"),
+					col=c("#81c0c5","#fb8335","#f9c8ea"))
+	axis(1, at=1:3,c("document nodes","attributes","text nodes"),las=2,cex.axis=.9)
+	
+	
+	par(mar=c(7,0,0.2,1.3)+.1)
+	# 	oldpar=par(mar=c(2,6.5,0.2,0)+.1,mfrow=c(1,1))
+	boxplot(nodesCellML,attributesCellML,textsCellML,horizontal=F,log='y',ylim=c(1,1000),
+					#names=c("updates","deletes","inserts","moves","nodes","attributes","texts"),
+					col=c("#81c0c5","#fb8335","#f9c8ea"), yaxt='n')
+	axis(1, at=1:3,c("document nodes","attributes","text nodes"),las=2,cex.axis=.9)
+	
+	
+	
+	
+	
+	par(oldpar)
+	
+	dev.off()
+	
+}
+
+
+
+
+
+
 dir.create("./data/graphs/boxes/", showWarnings=F, recursive=T)
 
 boxit (diffs, "all")
@@ -69,3 +176,4 @@ stopifnot (length(diffs[,1]) == length(SBML[,1]) + length (CellML[,1]))
 boxit (SBML, "SBML")
 boxit (CellML, "CellML")
 
+allinone (SBML, CellML)
